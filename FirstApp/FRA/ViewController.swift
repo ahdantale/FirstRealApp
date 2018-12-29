747caac6eb9a74eb6072cb29691f804e1a62f102
Abhishek Dantale <abhishekdantale@Abhisheks-MacBook-Pro.local>
2018-12-29 20:11:29 +0530

    ver 0.01

---
+++ FirstApp/FRA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstRealApp
+//
+//  Created by Abhishek Dantale on 29/12/18.
+//  Copyright © 2018 Abhishek Dantale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
